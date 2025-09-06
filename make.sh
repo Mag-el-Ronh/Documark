00e1cfa65472f5b64c520e51d503897a8d3279d4
Daniel <mag.el.ronh@gmail.com>
2025-09-06 23:32:15 +0200

    Update make.sh
    Dummy_Seat_Belt_Positioning_Automation_EN.html

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #! bash
 ## argument 1: output name of html document
 ## arguments 2+: list of required  markdown or html articles in html body
-python documark/main.py example.html content/front-page.md content/main-article.md
+python documark/main.py example.html draft_summery_dummy_seater/style_01.css draft_summery_dummy_seater/front_page.md draft_summery_dummy_seater/Proposal_Automation_Tool_Dummy_Seat_Belt_Positioning_EN.md draft_summery_dummy_seater/Dummy_Seat_Belt_Positioning_Automation_EN.html

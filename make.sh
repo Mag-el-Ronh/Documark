f683caf94e006efc76838710e6ab44b6b686a432
Daniel <mag.el.ronh@gmail.com>
2025-09-06 23:27:34 +0200

    Update make.sh
    example start command with shot argument infos

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
 #! bash
-## 
+## argument 1: output name of html document
+## arguments 2+: list of required  markdown or html articles in html body
 python documark/main.py example.html content/front-page.md content/main-article.md
